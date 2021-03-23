32a0f42d7425533916b7a723397d11ddd3f6522c
Ahmed Mahmoud <ahmed.mahmoud8116@gmail.com>
2021-03-23 17:46:23 +0200

    after models

---
+++ VacationForm/DBContexts/VacationContext.cs
@@ -0,0 +1,33 @@
+﻿using VacationForm.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace VacationForm.DBContexts
+{
+    public class VacationContext : DbContext
+    {
+        public VacationContext(DbContextOptions<VacationContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeBalance> EmployeeBalance { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Vacation> Vacations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Map entities to tables  
+            modelBuilder.Entity<Employee>().ToTable("Employee");
+            modelBuilder.Entity<EmployeeBalance>().ToTable("EmployeeBalance");
+            modelBuilder.Entity<Request>().ToTable("Request");
+            modelBuilder.Entity<Vacation>().ToTable("Vacation");
+
+
+        }
+        
+
+    }
+}
---
+++ VacationForm/Data/IVacationFormRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VacationForm.Models;
+namespace VacationForm.Data
+{
+    //this is what the app will provide
+    interface IVacationFormRepo
+    {
+        IEnumerable<Employee> GetAppEmployees();
+
+        //return type   name of function
+        Employee GetEmployeeByID(int id);
+    }
+}
